a0f3c8507d04b4062fcfd66dabc22fb15d0db598
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-06-28 12:36:05 +0200

    Creating 'trades' main model

--- trades/models.py
+++ trades/models.py
@@ -2,4 +2,24 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+# Just G3 currencies for now
+EURO = 'EUR'
+POUND = 'GBP'
+DOLLAR = 'USD'
+
+CURRENCIES = (
+    (EURO, 'EUR'),
+    (POUND, 'GBP'),
+    (DOLLAR, 'USD'),
+)
+
+
+class Trades(models.Model):
+
+    ID = models.CharField(max_length=9, primary_key=True)
+    sell_currency = models.CharField(max_length=3, choices=CURRENCIES)
+    sell_amount = models.DecimalField(max_digits=9, decimal_places=2)
+    buy_currency = models.CharField(max_length=3, choices=CURRENCIES)
+    buy_amount = models.DecimalField(max_digits=9, decimal_places=2)
+    rate = models.FloatField(default="")
+    date_booked = models.DateTimeField(auto_now_add=True)   # Automatic generation when created
