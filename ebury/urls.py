0c31e75ba84042c6687eb128d05d6551b8c97d31
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-06-28 12:10:33 +0200

    Adding basic home page

--- ebury/urls.py
+++ ebury/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from trades import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home)
 ]
