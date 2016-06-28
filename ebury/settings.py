95316286ad49a8dd4d4bf369aef2634fded6189f
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-06-28 11:44:44 +0200

    Migrate db

--- ebury/settings.py
+++ ebury/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'es-es'
+LANGUAGE_CODE = 'en-gb'
 
 TIME_ZONE = 'Europe/Madrid'
 
