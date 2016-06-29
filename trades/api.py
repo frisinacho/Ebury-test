55f00edc24f24f63feab2281c59e5e401fde0da4
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-06-29 17:17:15 +0200

    Showing different fields in List and Detail

--- trades/api.py
+++ trades/api.py
@@ -1,11 +1,13 @@
 from trades.models import Trades
-from trades.serializers import TradeSerializer
+from trades.serializers import TradeSerializer, TradeListSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 
 class TradeListAPI(ListCreateAPIView):
     queryset = Trades.objects.all()
-    serializer_class = TradeSerializer
+
+    def get_serializer_class(self):
+        return TradeSerializer if self.request.method == 'POST' else TradeListSerializer
 
 
 class TradeDetailAPI(RetrieveUpdateDestroyAPIView):
--- trades/serializers.py
+++ trades/serializers.py
@@ -7,3 +7,9 @@ class TradeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Trades
+
+
+class TradeListSerializer(TradeSerializer):
+
+    class Meta(TradeSerializer.Meta):
+        fields = ('sell_currency', 'sell_amount', 'buy_currency', 'buy_amount', 'rate', 'date_booked')
