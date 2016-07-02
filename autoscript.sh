eaf416ebc8cac215bf86dbbbe6189e5706fcec00
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-07-02 18:52:18 +0200

    Improvement in the autoscript

--- autoscript.sh
+++ autoscript.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+
+# First, it ensures that the database exists (if not, it creates an empty one with the models)
+env/bin/python manage.py migrate
+env/bin/python manage.py makemigration
+
+# Then, it runs the Django server
 env/bin/python manage.py runserver
