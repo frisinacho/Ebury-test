ab7efab1432a3679555b077413f87e50ed3cbb71
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-07-03 19:13:19 +0200

    Typo fixed

--- autoscript.sh
+++ autoscript.sh
@@ -2,7 +2,7 @@
 
 # First, it ensures that the database exists (if not, it creates an empty one with the models)
 env/bin/python manage.py migrate
-env/bin/python manage.py makemigration
+env/bin/python manage.py makemigrations
 
 # Then, it runs the Django server
 env/bin/python manage.py runserver
